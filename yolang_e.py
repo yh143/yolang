acffc95f8374f2c83cd8152d1c388a944c8f5622
yh143 <13887283268@163.com>
2026-01-01 11:47:17 +0800

    Add files via upload

--- yolang_e.py
+++ yolang_e.py
@@ -4,16 +4,29 @@ print("Some codes is AI builded,maybe have some bugs or funny things")
 while True:
     codes=input("Enter your YoLang code:")
     def textshow(input_text):
-        print(input_text)
+        text=input('show text:')
+        print(text)
     #show text
+    def numbershow():
+        num1=input('show number:')
+        print(eval(num1))
+    #show number
     def textinput(prompt_text):
         return input(prompt_text)
     #input text
-    def numberinput(prompt_text):
-        num1=input(prompt_text)
-        return eval(num1)
+    #check codes for run
     if "textshow(" in codes:
         start_index=codes.index("textshow(")+9
         end_index=codes.index(")",start_index)
         content=codes[start_index:end_index]
         textshow(content)
+    elif "numbershow()" in codes:
+        numbershow()
+    elif "textinput()" in codes:
+        start_index=codes.index("textinput(")+10
+        end_index=codes.index(")",start_index)
+        prompt=codes[start_index:end_index]
+        result=textinput(prompt)
+        print(result)
+    else:
+        print("Unknown YoLang Command")
